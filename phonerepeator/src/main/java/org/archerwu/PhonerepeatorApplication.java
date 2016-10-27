638da79b61c0d53ce191c9be1cb400e531198b74
ArcherCYWu <archer.cy.wu@gmail.com>
2016-10-27 23:17:57 +1100

    first commit from local

---
+++ phonerepeator/src/main/java/org/archerwu/PhonerepeatorApplication.java
@@ -0,0 +1,12 @@
+package org.archerwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonerepeatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonerepeatorApplication.class, args);
+	}
+}
